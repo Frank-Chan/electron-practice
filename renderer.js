d53a89bc2e58b1c2ac321dceb14235017525898c
Frank-Chan <gisfeifly@gmail.com>
2024-08-16 11:50:19 +0800

    Feat:新打开的窗口设置为屏幕大小

--- renderer.js
+++ renderer.js
@@ -8,9 +8,17 @@ document.getElementById("loadRenderFile").addEventListener("click", () => {
 document.getElementById("openBaiDu").addEventListener("click", () => {
   // 向渲染进程发送消息
   //   window.ipcRenderer.send('message-from-renderer', 'Hello from renderer.js');
-  // 打开百度,并将新打开的窗口设置为新的窗口
+
+  const width = window.screen.width;
+  const height = window.screen.height;
+  // 打开百度,并将新打开的窗口设置为新的窗口，同时设置窗口的大小为屏幕大小
+  window.open(
+    "https://www.baidu.com",
+    "_blank",
+    `width=${width},height=${height}`
+  );
   // 打开百度,并将新打开的窗口设置为新的窗口，同时设置窗口的显示位置和大小
-  window.open("https://www.baidu.com", "_blank", "width=1200px,height=900px");
-  
-//   window.open("https://www.baidu.com");
+  // window.open("https://www.baidu.com", "_blank", "width=1200px,height=900px");
+
+  //   window.open("https://www.baidu.com");
 });
